b998caf0471881ba17b08b8f468d97c1913f9b24
Kevin Zhu <kzhu2099@gmail.com>
2025-03-27 11:22:53 -0500

    complete

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,28 @@
+'''
+Author: Kevin Zhu
+A basic example for the myweather library,
+'''
+
 from myweather import MyWeather
-import json
 
 if __name__ == '__main__':
-    w = MyWeather()
+    w = MyWeather('your_email@example.com', 'your appp pass word')
+    w.set_location_name('New York')
 
-    weather = w.get_current_location_weather('Chicago')
+    '''
+    1 week forecast (the max)
+    '''
 
-    print(weather)
+    forecast = w.get_forecast(days = 7, skip_nights = False)
+    html = w.forecast_to_html(forecast)
+    print(html)
+
+    '''
+    12 hour forecast
+    '''
+
+    forecast = w.get_hourly_forecast(hours = 12)
+    html = w.forecast_to_html(forecast)
+    print(html)
+
+    w.send_email('your_email@gmail.com', timezone = 'US/Eastern')
